9862cffde7652e68b850b7d43822800111448f4c
Ethan Tecson <ethantecson@gmail.com>
2024-03-28 21:14:00 -0400

    insert() working. in_order in the works.

--- bst.cpp
+++ bst.cpp
@@ -2,6 +2,59 @@
 #include <iostream>
 #include <sstream>
 
+//===================================
+// Node Class
+//===================================
+template <typename Data, typename Key>
+Node<Data,Key>::Node(){
+    left = nullptr;
+    right = nullptr;
+    parent = nullptr;
+}
+
+
+template <typename Data, typename Key>
+Node<Data,Key>::Node(Data d, Key k){
+
+    data = d;
+    key = k;
+
+    left = nullptr;
+    right = nullptr;
+    parent = nullptr;
+}
+
+template <typename Data, typename Key>
+Node<Data, Key>::~Node() {
+    // Destructor implementation goes here
+    // For example, you can delete any dynamically allocated memory
+    // or perform any necessary cleanup.
+}
+
+
+/**
+ * Delete Node function
+ *
+ *
+ *
+ *
+ */
+ template <typename Data, typename Key>
+ void Node<Data, Key>::deleteNode(Node<Data, Key>* node){ //Havent tested since we have no insert func
+    // If root isn't empty
+    if(node != nullptr){
+        // Recursively go into the children of the parent node
+        deleteNode(node->left);
+        deleteNode(node->right);
+
+        // Delete them
+        delete node;
+    }
+ }
+
+//===================================
+// BST Class
+//===================================
 /**
  * BST constructor
  *
@@ -14,25 +67,7 @@ BST<Data, Key>::BST() {
     root = nullptr;
 }
 
-/**
- * Delete Node function
- *
- *
- *
- *
- */
- template <typename Data, typename Key>
- void Node<Data, Key>::deleteNode(Node<Data, Key>* node){ //Havent tested since we have no insert func
-    // If root isn't empty
-    if(!node = nullptr){
-        // Recursively go into the children of the parent node
-        deleteNode(node->left);
-        deleteNode(node->right);
 
-        // Delete them
-        delete;
-    }
- }
 
 /**
  * BST deconstructor
@@ -43,7 +78,9 @@ BST<Data, Key>::BST() {
  */
 template <typename Data, typename Key>
 BST<Data, Key>::~BST() {
-    deleteNode(root);
+    if (root != nullptr) {
+        root->deleteNode(root);
+    }
 }
 
 
@@ -56,25 +93,25 @@ BST<Data, Key>::~BST() {
  */
 template <typename Data, typename Key>
 void BST<Data, Key>::insert(Data data, Key key) {
-    // if(root == nullptr){
-    //     root = Node(data, key);
-    // }else{
-    //     Node *temp = root;
-    //     Node *parent = nullptr;
-    //     while(temp != nullptr){
-    //         parent = temp;
-    //         if(key < temp.key){
-    //             temp = temp.left;
-    //         }else{
-    //             temp = temp.right;
-    //         }
-    //     }
-    //     if(key < parent){
-    //         parent.left = Node(data, key);
-    //     }else{
-    //         parent.right = Node(data, key);
-    //     }
-    // }
+    if(root == nullptr){
+        root = new Node<Data, Key>(data, key);
+    }else{
+        Node<Data, Key> *temp = root;
+        Node<Data, Key> *parent = nullptr;
+        while(temp != nullptr){
+            parent = temp;
+            if(key < temp->key){
+                temp = temp->left;
+            }else{
+                temp = temp->right;
+            }
+        }
+        if(key < parent->key){
+            parent->left = new Node<Data, Key>(data, key);
+        }else{
+            parent->right = new Node<Data, Key>(data, key);
+        }
+    }
 }
 
 /**
@@ -84,18 +121,18 @@ void BST<Data, Key>::insert(Data data, Key key) {
  *
  *
  */
-template <typename Data, typename Key>
-Data BST<Data, Key>::get(Key key) const {
-    Node *temp = root;
-    while(temp != nullptr && temp->key != key){
-        if(key < temp){
-            temp = temp->left;
-        }else{
-            temp = temp->right;
-        }
-    }
-    return temp->data;
-}
+// template <typename Data, typename Key>
+// Data BST<Data, Key>::get(Key key) const {
+//     Node *temp = root;
+//     while(temp != nullptr && temp->key != key){
+//         if(key < temp){
+//             temp = temp->left;
+//         }else{
+//             temp = temp->right;
+//         }
+//     }
+//     return temp->data;
+// }
 
 /**
  * remove function
@@ -154,7 +191,7 @@ Data BST<Data, Key>::min_data() const {
  */
 template <typename Data, typename Key>
 Key BST<Data, Key>::min_key() const {
-    Node *temp = root;
+    Node<Data, Key> *temp = root;
     while(temp->left != nullptr){
         temp = temp->left;
     }
@@ -182,7 +219,7 @@ Key BST<Data, Key>::successor(Key key) const {
  */
 template <typename Data, typename Key>
 string BST<Data, Key>::in_order() const {
-    
+    return in_order_tree_walk(root);
 }
 
 /**
@@ -192,10 +229,10 @@ string BST<Data, Key>::in_order() const {
  *
  *
  */
-template <typename Data, typename Key>
-void BST<Data, Key>::trim(Key key1, Key key2) {
+// template <typename Data, typename Key>
+// void BST<Data, Key>::trim(Key key1, Key key2) {
     
-}
+// }
 
 /**
  * to_string function
@@ -204,8 +241,46 @@ void BST<Data, Key>::trim(Key key1, Key key2) {
  *
  *
  */
+
+// template <typename Data, typename Key>
+// string BST<Data, Key>::to_string() const {
+
+//     // stringstream ss;
+
+//     // if root == nullptr; 
+//     // {
+//     //     return ss.to_str();
+//     // }
+
+//     // ss << root->key << " ";
+
+//     // Node *curr = root;
+
+//     // while curr != nullptr{ // Get to smallest key
+//     //     if (curr->left == nullptr){
+
+//     //     }
+//     //     ptr = curr->left;
+//     // } 
+//     // ss << curr->key;
+// }
+
+// template <typename Data, typename Key>
+// string BST<Data, Key>::in_order_tree_walk(Node<Data, Key> *x) const{
+//      if (x != nullptr){
+//         in_order_tree_walk(x->left);
+//         cout << x->data;
+//         in_order_tree_walk(x->right);
+//      }
+// }
+
 template <typename Data, typename Key>
-string BST<Data, Key>::to_string() const {
-    
-    
+string BST<Data, Key>::in_order_tree_walk(Node<Data, Key> *x) const {
+    stringstream ss;
+    if (x != nullptr) {
+        ss << in_order_tree_walk(x->left);
+        ss << x->key << " ";
+        ss << in_order_tree_walk(x->right);
+    }
+    return ss.str();
 }
